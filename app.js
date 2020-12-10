36dfa16949cb5d513bc8b421d46fd796b74b9867
BHARADWAJ Saurabh <g513310@mph.morpho.com>
2020-12-10 09:45:15 +0530

    10-Dec-2020 before bcrypt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const { stringify } = require("querystring");
 
@@ -19,12 +20,13 @@ app.use(bodyParser.urlencoded({
 }));
 
 mongoose.connect("mongodb://localhost:27017/skillManagement",{useNewUrlParser:true,useUnifiedTopology: true});
+
 const userSchema =new mongoose.Schema({
 email :String,
 password:String
 });
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
 
 const User = new mongoose.model("User",userSchema); 
 
@@ -58,7 +60,7 @@ app.post("/register",function(req,res){
                         {
                             const newUser = new User({
                                 email:req.body.username,
-                                password:req.body.password
+                                password:md5(req.body.password)
                             });
 
                             newUser.save(function(err){
@@ -78,7 +80,7 @@ app.post("/register",function(req,res){
 
 app.post("/login1",function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email:username},function(err,founduser){
         if(err) {
             console.log(err +"Saurabh");
